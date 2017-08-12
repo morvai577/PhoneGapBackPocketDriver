c3d4df23ad32c35aa731425ae5716a13df114de0
Vai Mor <mor.vaibhav577@gmail.com>
2017-08-12 21:29:22 +1200

    Copy functional homescreen code from webapp to this phoneGap project

--- BackPocketDriver/platforms/browser/www/js/script.js
+++ BackPocketDriver/platforms/browser/www/js/script.js
@@ -1,24 +1,22 @@
-document.addEventListener("deviceready", sidebar, false);
-
 // Global Variables
 var driving = false;
 var sideBarOpen = false;
 
-function sidebar() {
-  var open = $('.open-nav'),
-      overlay = $('.overlay'),
-      close = $('.close');
+$(document).ready(function() {
+    var open = $('.open-nav'),
+        overlay = $('.overlay'),
+        close = $('.close');
 
-  open.click(function() {
-      overlay.show();
-      $('#wrapper').addClass('toggled');
-  });
+    open.click(function() {
+        overlay.show();
+        $('#wrapper').addClass('toggled');
+    });
 
-  overlay.click(function() {
-    overlay.hide();
-    $('#wrapper').removeClass('toggled');
-  })
-}
+    overlay.click(function() {
+      overlay.hide();
+      $('#wrapper').removeClass('toggled');
+    })
+});
 
 function startJourney() {
   if (sideBarOpen === false) {
